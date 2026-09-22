bf3fcca3fdaa5d49fc3386e1911a2b635fb190da
zeyu.wang <zeyu.wang@dyness-tech.com>
2026-09-22 17:38:03 +0800

    add redis

--- src/main/java/com/zy/traveltrip/TravelTripApplication.java
+++ src/main/java/com/zy/traveltrip/TravelTripApplication.java
@@ -1,5 +1,6 @@
 package com.zy.traveltrip;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,9 +9,11 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @EnableDiscoveryClient
 @SpringBootApplication
+@Slf4j
 public class TravelTripApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TravelTripApplication.class, args);
+        log.info("服务启动成功！");
     }
 }
